de3c617289de0f553f16080a8d0199d35ebc01fd
Charles-Roro <charlesrobertmanning@gmail.com>
2025-05-15 15:45:30 -0500

    New branch to test pull request for fun

--- 03-subnets.tf
+++ 03-subnets.tf
@@ -5,3 +5,11 @@ resource "google_compute_subnetwork" "private" {
   network                  = google_compute_network.main.id
   private_ip_google_access = true
 }
+
+resource "google_compute_subnetwork" "private-cali" {
+  name                     = "private-cali"
+  ip_cidr_range            = "10.1.0.0/24"
+  region                   = "us-west1"
+  network                  = google_compute_network.main.id
+  private_ip_google_access = true
+}
